0abf575ed53cbf091d9b1038af446443e2e73f43
Igor <42407016+everyvoid@users.noreply.github.com>
2023-06-27 09:27:57 +0200

    physics config updated

--- Assets/Scripts/KeypressSoundTrigger.cs
+++ Assets/Scripts/KeypressSoundTrigger.cs
@@ -5,33 +5,46 @@ using System.Collections;
  public class KeypressSoundTrigger : MonoBehaviour {
  
   public AudioSource suggestRecipes1;
-  public AudioSource wasteWarning2;
-  public AudioSource groceryOrdering3;
+  public AudioSource selectionRecipe2;
+  public AudioSource confirmationRecipe3;
+  public AudioSource groceryOrdering4;
 
   // Update is called once per frame
   void Update () {
          if (Input.GetKeyDown(KeyCode.Alpha1)) {
              if(!suggestRecipes1.isPlaying &&
-                     !wasteWarning2.isPlaying &&
-                     !groceryOrdering3.isPlaying) {
+                     !selectionRecipe2.isPlaying &&
+                     !confirmationRecipe3.isPlaying &&
+                     !groceryOrdering4.isPlaying) {
                  suggestRecipes1.Play();
              }
          }
       
          if (Input.GetKeyDown(KeyCode.Alpha2)) {
              if(!suggestRecipes1.isPlaying &&
-                     !wasteWarning2.isPlaying &&
-                     !groceryOrdering3.isPlaying) {
-                 wasteWarning2.Play();
+                     !selectionRecipe2.isPlaying &&
+                     !confirmationRecipe3.isPlaying &&
+                     !groceryOrdering4.isPlaying) {
+                 selectionRecipe2.Play();
              }
          }
 
 
          if (Input.GetKeyDown(KeyCode.Alpha3)) {
              if(!suggestRecipes1.isPlaying &&
-                     !wasteWarning2.isPlaying &&
-                     !groceryOrdering3.isPlaying) {
-                 groceryOrdering3.Play();
+                     !selectionRecipe2.isPlaying &&
+                     !confirmationRecipe3.isPlaying &&
+                     !groceryOrdering4.isPlaying) {
+                 confirmationRecipe3.Play();
+             }
+         }
+
+         if (Input.GetKeyDown(KeyCode.Alpha4)) {
+             if(!suggestRecipes1.isPlaying &&
+                     !selectionRecipe2.isPlaying &&
+                     !confirmationRecipe3.isPlaying &&
+                     !groceryOrdering4.isPlaying) {
+                 groceryOrdering4.Play();
              }
          }
      }
